99d54fed61ee59d2991ac219c6435802cc1b66e1
n0nst1ck <92935631+n0nst1ck@users.noreply.github.com>
2022-02-05 13:45:14 +0200

    Update exercise13.py

--- exercise13.py
+++ exercise13.py
@@ -1,7 +1,7 @@
 # Exercise 13
 import requests
 import re
-
+# Getting the latest randomness
 r=requests.get('https://drand.cloudflare.com/public/latest', headers={
 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 \
 Firefox/31.0'})
@@ -10,15 +10,15 @@ a=str(data).split(",")
 a=a[1].split("'")
 randomness=a[3]
 print("Latest randomness: ", randomness)
-
+# Splitting randomness
 seq=re.findall('.{1,2}', randomness)
 for i in range(len(seq)):
     seq[i]=(int(seq[i], 16))%80
-
+# Getting kino data
 r=requests.get('https://api.opap.gr/draws/v3.0/1100/last-result-and-active')
 data=r.json()
 win=((data.get("last")).get("winningNumbers")).get("list")
-
+# Finding intersection
 set1=set(seq)
 inter=set1.intersection(win)
 print("The amount of numbers that would win are:", len(list(inter)))
